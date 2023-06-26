fc00da144282eb78e74eabfe5e86b7c217f67b3f
Florent Vasseur <vasseurflorent@gmail.com>
2023-06-26 18:58:56 +0200

    super user

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band, Listing
+
+class BandAdmin(admin.ModelAdmin):
+  list_display = ('name', 'genre', 'year_formed', 'active')
+  list_filter = ('genre', 'active')
+  search_fields = ('name', 'genre', 'year_formed', 'active')
+
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('title', 'year', 'price', 'sold', 'type')
+  list_filter = ('year', 'price', 'sold')
+  search_fields = ('title', 'year', 'price', 'sold')
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
